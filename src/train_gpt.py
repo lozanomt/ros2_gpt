14bf5ae03ea5123de552435ee404bc2468efcbd9
lozanomt <lozanomt@vcu.edu>
2024-04-02 14:18:39 -0400

    .

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -94,8 +94,6 @@ def train_gpt(train_data_loader, num_epochs=3, model_name_or_path='gpt2', output
 
 
 # Instantiate your Dataset
-print(os.getcwd())
-print(ROOT)
 data_path = f"{ROOT}/data/test_data.txt"
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 max_length = 512
