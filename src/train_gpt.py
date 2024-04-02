2a0dc0e15ba75c381fc3c7b1456a19750e6eddd2
lozanomt <lozanomt@vcu.edu>
2024-04-02 14:06:17 -0400

    data folder added

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import torch
 import os
+import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from torch.utils.data import DataLoader, Dataset
 from transformers import get_linear_schedule_with_warmup
@@ -86,7 +86,7 @@ def train_gpt(train_data_loader, num_epochs=3, model_name_or_path='gpt2', output
 
 
 # Instantiate your Dataset
-data_path = "src/gpt_training/data/test.txt"
+data_path = "src/gpt_training/data/test_data.txt"
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 max_length = 512
 
