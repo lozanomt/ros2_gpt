87683013413128411b7587d4fd7259408a2e8ff5
lozanomt <lozanomt@vcu.edu>
2024-04-03 11:26:33 -0400

    .

---
+++ src/output_subscriber_node.cpp
@@ -0,0 +1,31 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class SubscriberNode : public rclcpp::Node
+{
+public:
+    SubscriberNode() : Node("subscriber_node")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "message_bus",
+            10,
+            std::bind(&SubscriberNode::topicCallback, this, std::placeholders::_1));
+    }
+
+private:
+    void topicCallback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received: %s", msg->data.c_str());
+    }
+
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SubscriberNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
